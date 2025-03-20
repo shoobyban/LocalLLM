787c7f4939973ed68b0b23f1b74756442cc0c5aa
Tech <architect@alentr.com>
2025-03-20 11:10:17 +0000

    Initial Commit

---
+++ LocalLLM/LocalLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLLMApp.swift
+//  LocalLLM
+//
+//  Created by Sam Ban on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalLLMTests/LocalLLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLLMTests.swift
+//  LocalLLMTests
+//
+//  Created by Sam Ban on 20/03/2025.
+//
+
+import Testing
+@testable import LocalLLM
+
+struct LocalLLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
